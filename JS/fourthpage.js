974ddc24c8c55524173743972aebc1bb43beffdd
Trisha <48178740+trishapelagio@users.noreply.github.com>
2020-08-18 16:02:32 +0800

    fixed script

---
+++ JS/fourthpage.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#code").keyup(function(){
+        var val = $("#code").val();
+        if(val == "secret14976444"){
+            window.location.replace("fasdga.html");
+        }
+    })
+});
